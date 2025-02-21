3accb2e0105d48686d560e9e4bf2dddc056f5ac4
faith <faith@marascientific.org>
2025-02-21 13:28:31 +0300

    Add python implementation

--- app.py
+++ app.py
@@ -1,45 +1,93 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
+from datetime import date
+from flask import request
+
 
 
 app = Flask(__name__)
 
 
-#create post
-@app.route('/blog/add',methods=["POST"])
-def create_post():
-    #logic for creating blog post here
-    return 'Blog created successfully'
-
-#get blog post
-@app.route('/blog')
-def get_all_posts():
-    blogs = [{
-        "title":"First blog",
-        "content":"Test first blog"
+@app.route('/')
+def home():
+    
+    today = date.today()
+    posts = [{
+        "id":1,
+        "title":"First post",
+        "content":"Post content goes here",
+        "author":"John Doe",
+        "date":today
+    },
+    {
+        "id":2,
+        "title":"First post",
+        "content":"Post content goes here",
+        "author":"John Doe",
+        "date":today
+    },
+      {
+        "id":3,
+        "title":"First post",
+        "content":"Post content goes here",
+        "author":"John Doe",
+        "date":today
     }]
-    return jsonify(blogs)
+    return render_template("index.html", posts=posts)
 
-#get single post
-@app.route('/blog/<int:post_id>')
-def get_single_post(post_id):
-    blog = {
+@app.route('/posts')
+def posts():
+    today = date.today()
+    posts = [{
+        "id":1,
+        "title":"First post",
+        "content":"Post content goes here",
+        "author":"John Doe",
+        "date":today
+    },
+    {
+        "id":2,
+        "title":"First post",
+        "content":"Post content goes here",
+        "author":"John Doe",
+        "date":today
+    },
+    {
+        "id":3,
+        "title":"First post",
+        "content":"Post content goes here",
+        "author":"John Doe",
+        "date":today
+    },
+        {
+        "id":4,
+        "title":"First post",
+        "content":"Post content goes here",
+        "author":"John Doe",
+        "date":today
+    }]
+    return render_template("posts.html", posts=posts)
+
+@app.route('/new_post', methods=["POST","GET"])
+def new_post():
+    if request.method == "GET":
+        return render_template("new_post.html")
+    else:
+        return "Save post implementation"
+
+@app.route('/post/<int:post_id>')
+def view_post(post_id):
+    today = date.today()
+    post = {
         "id":post_id,
-        "title":"Blog one",
-        "content":"Blog one content"
+        "title":"First post",
+        "content":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sit amet blandit massa, ac bibendum lectus. Sed molestie rutrum risus in imperdiet. Nullam imperdiet felis ante, a semper metus molestie at. Pellentesque cursus nisi vitae libero eleifend convallis. Fusce",
+        "author":"John Doe",
+        "date":today
     }
-    return jsonify(blog)
+    return render_template("post.html", post=post)
 
-#update post
-@app.route('/blog/<int:post_id>', methods=["PUT"])
-def update_post(post_id):
-    return f'Blog post #{post_id} updated successfully'
-
-#delete post
-@app.route('/blog/<int:post_id>', methods=["DELETE"])
-def delete_post(post_id):
-    return f'Blog post #{post_id} deleted successfully'
 
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
