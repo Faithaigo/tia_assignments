20cc80c325272202b45232eb0085e6b55a825375
faith <faith@marascientific.org>
2025-03-04 12:27:29 +0300

    Add labels

--- forms.py
+++ forms.py
@@ -4,6 +4,6 @@ from wtforms.validators import DataRequired
 
 
 class PostForm(FlaskForm):
-    title = StringField(validators=[DataRequired()])
-    content = TextAreaField(validators=[DataRequired()])
-    author = StringField(validators=[DataRequired()])
+    title = StringField('Title',validators=[DataRequired()])
+    content = TextAreaField('Content',validators=[DataRequired()])
+    author = StringField('Author',validators=[DataRequired()])
