cc87d7c095f209dc8690267b363e28827e5afa57
faith <faith@marascientific.org>
2025-03-04 11:55:23 +0300

    Add models

---
+++ models.py
@@ -0,0 +1,19 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+class Base(DeclarativeBase):
+  pass
+
+db = SQLAlchemy(model_class=Base)
+
+class Posts(db.Model):
+    id:Mapped[int] = mapped_column(primary_key=True)
+    title:Mapped[str]
+    content:Mapped[str]
+    author:Mapped[str]
+    
+class User(db.Model):
+    id:Mapped[int] = mapped_column(primary_key=True)
+    full_name:Mapped[str]
+    email:Mapped[str] = mapped_column(unique=True)
+    password:Mapped[str]
