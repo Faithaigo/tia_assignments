29f5aec0fa219b083b24013b07326096dc1790ac
faith <faith@marascientific.org>
2025-03-04 13:27:24 +0300

    Add register form

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, TextAreaField, SubmitField, EmailField, PasswordField
+from wtforms.validators import DataRequired,EqualTo
 
 
 class PostForm(FlaskForm):
@@ -8,3 +8,11 @@ class PostForm(FlaskForm):
     content = TextAreaField('Content',validators=[DataRequired()])
     author = StringField('Author',validators=[DataRequired()])
     submit = SubmitField("Submit")
+    
+    
+class RegisterForm(FlaskForm):
+    full_name = StringField('Name',validators=[DataRequired()])
+    email = EmailField('Email',validators=[DataRequired()])
+    password = PasswordField('Password',validators=[DataRequired()])
+    password = PasswordField('Confirm password',validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField("Register")
