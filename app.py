5f55e85c165a01035a1457e5d14ad621becb3c36
faith <faith@marascientific.org>
2025-03-16 00:12:00 +0300

    Register posts blueprint

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from extensions import db, bcrypt
 from dotenv import load_dotenv
 from auth.auth_blueprint import auth_blueprint
 from auth.models import User
+from posts.posts_blueprint import posts_blueprint
 
 
 
@@ -16,8 +17,7 @@ app = Flask(__name__)
 app.secret_key = os.getenv("LOGIN_SECRET_KEY")
 
 app.register_blueprint(auth_blueprint)
-
-
+app.register_blueprint(posts_blueprint)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql+psycopg2://{os.getenv("DB_USER")}:{os.getenv("DB_PASSWORD")}@{os.getenv("DB_HOST")}:{os.getenv("DB_PORT")}/{os.getenv("DB_NAME")}"
 
