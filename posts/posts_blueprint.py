f47a19798a8a38559ad96c1dd26f613cb6ff435a
faith <faith@marascientific.org>
2025-03-31 12:40:33 +0300

    Add logging

--- posts/posts_blueprint.py
+++ posts/posts_blueprint.py
@@ -1,3 +1,4 @@
+import logging
 from flask import Blueprint, jsonify
 from flask_login import login_required, current_user
 from flask_restful import Api, Resource, reqparse
@@ -9,6 +10,8 @@ from extensions import db
 posts_blueprint = Blueprint('posts', __name__, url_prefix="/blog")
 
 api = Api(posts_blueprint)
+
+logger = logging.getLogger(__name__)
 
 parser = reqparse.RequestParser()
 parser.add_argument('title', type=str, help="Title of post")
@@ -27,33 +30,45 @@ def unauthorized_access(e):
 class BlogPosts(Resource):
     @login_required
     def get(self):
-        all_posts = db.session.execute(db.select(Posts)).scalars().all()
-        posts = []
-        for post in all_posts:
-            p = post.__dict__
-            posts.append(p)
-        for post in posts:
-            post.pop('_sa_instance_state', None)
-        return {"posts": posts}, 200
+        try:
+            all_posts = db.session.execute(db.select(Posts)).scalars().all()
+            posts = []
+            for post in all_posts:
+                p = post.__dict__
+                posts.append(p)
+            for post in posts:
+                post.pop('_sa_instance_state', None)
+            return {"posts": posts}, 200
+        except Exception as e:
+            logger.error('Error occurred: %s', str(e))
+            raise
 
     @login_required
     def post(self):
-        if not current_user:
-            raise InvalidAPIUsage('You are not authorized', status_code=401)
-        arg = parser.parse_args()
-        if not arg["title"]:
-            raise InvalidAPIUsage('Please provide a title')
-        added_post = Posts(title=arg["title"], content=arg["content"], author=arg["author"])
-        db.session.add(added_post)
-        db.session.commit()
-        return {"message": "Post added successfully"}, 200
+        try:
+            if not current_user:
+                raise InvalidAPIUsage('You are not authorized', status_code=401)
+            arg = parser.parse_args()
+            if not arg["title"]:
+                raise InvalidAPIUsage('Please provide a title')
+            added_post = Posts(title=arg["title"], content=arg["content"], author=arg["author"])
+            db.session.add(added_post)
+            db.session.commit()
+            return {"message": "Post added successfully"}, 200
+        except Exception as e:
+            logger.error('Error occurred: %s', str(e))
+            raise
 
 
 class BlogPost(Resource):
     @login_required
     def get(self, post_id):
-        post = db.get_or_404(Posts, post_id)
-        return {"id": post.id, "title": post.title, "content": post.content, "author": post.author}, 200
+        try:
+            post = db.get_or_404(Posts, post_id)
+            return {"id": post.id, "title": post.title, "content": post.content, "author": post.author}, 200
+        except Exception as e:
+            logger.error('Error occurred: %s', str(e))
+            raise
 
     @login_required
     def put(self, post_id):
