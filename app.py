4168826c731581a44856f923265957aac398152b
faith <faith@marascientific.org>
2025-03-31 11:35:31 +0300

    Add error handing for 404 and 405 methods

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, jsonify
 from flask_login import LoginManager
 from extensions import db, bcrypt
 from dotenv import load_dotenv
@@ -33,6 +33,13 @@ login_manager.init_app(app)
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+@app.errorhandler(404)
+def resource_not_found(e):
+    return jsonify(message=str(e), status=404), 404
+
+@app.errorhandler(405)
+def method_not_allowed(e):
+    return jsonify(message=str(e), status=405), 405
 
 with app.app_context():
     db.create_all()
